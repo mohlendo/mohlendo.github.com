9e8ab7277e76491fa69d89c0a75adc8efeb940a8
Manuel Ohlendorf <manuel.ohlendorf@coremedia.com>
2010-12-30 10:33:23 +0100

    new style

--- js/site.js
+++ js/site.js
@@ -1,19 +1,19 @@
 function paragraphPermalinks(){
-  $("div.blog_post span.post_prose").each(
+  $("article div").each(
        function(j) {
          $(this).children("p[id]").each(
             function(i) {
               p = $(this);
               a = $("<a/>");
               //Full href for when we are not on the permapage
-              href = p.parents("div.blog_post").find("h2.blog_post_title a").attr("href");
+              href = p.parents("article div").find("h2.blogPostTitle a").attr("href");
               if(document.documentURI.match(href)){
                 //If we are on the permapage, specify a relative url
                 //so as not to reload the page.
                 href = "";
               }
               a.attr('href',href+"#p"+(i+1));
-              a.attr('class','paragraph_permalink');
+              a.attr('class','paragraphPermalink');
               a.text(" ¶");
               a.attr("title","Link to this paragraph");
               p.append(a);
@@ -24,7 +24,7 @@ function paragraphPermalinks(){
 
 function twitterSidebar(){
   $("#mohlendo_tweets").tweet({
-      avatar_size: 32,
+      avatar_size: 16,
         count: 5,
         query: "mohlendo",
         loading_text: "searching twitter..."
